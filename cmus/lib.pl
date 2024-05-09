ff2db25d74354ddafdfd409bd1c8685732cd98d9
Drunkss <davidessien243@gmail.com>
2024-05-08 20:26:09 -0500

    init

---
+++ cmus/lib.pl
@@ -0,0 +1,7 @@
+/home/prince/Music/chromemusic.mp4
+/home/prince/Music/le-sy.mp3
+/home/prince/Music/LOBOTOMY KAISEN _ OFFICIAL TRAILER.mp4
+/home/prince/Music/neverland.mp3
+/home/prince/Music/soa.mp3
+/home/prince/Music/supon.mp3
+/home/prince/Music/UndertaleRE.mp3
---
+++ spotify/Users/31g5zug6mveumqp3jjimot2ndgau-user/frecency.pb
@@ -0,0 +1,5 @@
+
+K
+4spotify:user:31g5zug6mveumqp3jjimot2ndgau:collection	Í³µ`óx@ ™üÛ¦
+h
+'spotify:playlist:7diGbaffuxxk5JmtOhk9Hp	r^WV5y@ ­‡¯	a{ÇD!Gy@ ï‡¯	£B5y@ Û†¯
